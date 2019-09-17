01bd351581479ddc318e6a5d019b997180513129
Bjorn <bjorn.andersson@igotchastudios.com>
2019-09-17 02:24:13 +0200

    Localization working

---
+++ Assets/Scripts/Data/Constants.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Constants
+{
+    //Translations
+    #region Translations
+    public const string GameNameTranslation = "Game_Name";
+    #endregion
+
+    //Paths
+    #region Paths
+    public const string TranslationJsonName = "Translations.json";
+    public static readonly string LocalizationPath = $"{Application.dataPath}/Data/Localization/";
+    #endregion
+
+    //Defaults
+    #region Defaults
+    public const string DefaultLanguage = "en";
+    #endregion
+}
---
+++ Assets/Scripts/GameSystems/Localization/LocalizationManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+public class LocalizationManager : ILocalizationManager
+{
+    private TranslationCatalog _translationCatalog;
+
+    public LocalizationManager()
+    {
+        var userLanguage = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
+        if (!Directory.Exists(Path.Combine(Constants.LocalizationPath, userLanguage)))
+        {
+            userLanguage = Constants.DefaultLanguage;
+        }
+
+        var jsonContent = File.ReadAllText(
+            Path.Combine(Constants.LocalizationPath,
+            $"{userLanguage}/{Constants.TranslationJsonName}"));
+
+        var translationCatalog = new DataContractJsonSerializer(typeof(TranslationCatalog),
+            new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true
+            });
+
+        _translationCatalog = translationCatalog.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(jsonContent))) as TranslationCatalog;
+    }
+
+    public string GetTranslation(string translationId)
+    {
+        return _translationCatalog.Translations.ContainsKey(translationId) ? _translationCatalog.Translations[translationId] : translationId;
+    }
+}
