d68b5e68707874ff01fb4b44b9dc7f80f6578738
KazzTruul <kazztruul@hotmail.com>
2019-09-20 23:03:13 +0200

    Functionality for MainMenu localization added

---
+++ Assets/Scripts/GameSystems/DependencyInstallers/InitialSceneInstaller.cs
@@ -0,0 +1,32 @@
+﻿using Zenject;
+
+public class InitialSceneInstaller : MonoInstaller
+{
+    //Set up all bindings for Zenject dependency injection
+    public override void InstallBindings()
+    {
+        //Add Container to SignalBusInstaller
+        SignalBusInstaller.Install(Container);
+
+        //Declare signals
+        Container.DeclareSignal<LanguageChangedSignal>();
+
+        //Set up bindings
+        Container.Bind<SettingsContainer>()
+            .AsSingle()
+            .Lazy();
+        Container.Bind<ILocalizationManager>()
+            .To<LocalizationManager>()
+            .AsSingle()
+            .Lazy();
+        Container.Bind<ICommandHandler>()
+            .To<CommandHandler>()
+            .AsSingle()
+            .Lazy();
+
+        //Bind signals
+        Container.BindSignal<LanguageChangedSignal>()
+            .ToMethod<SettingsContainer>(settingsContainer => settingsContainer.SetLanguage)
+            .FromResolve();
+    }
+}
